60d3c1dbd3574c18431403dd1cde6979c3a8c71f
wolfram74 <phaugen@gmail.com>
2015-05-11 11:43:20 -0700

    Refactored some of the javascript

--- js/text_response.js
+++ js/text_response.js
@@ -1,3 +1,45 @@
+if (!String.prototype.repeat) {
+  String.prototype.repeat = function(count) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count != count) {
+      count = 0;
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+      if ((count & 1) == 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    return rpt;
+  }
+}
+
 $(document).ready(function(){
   $("#submit_text").on("click", buttonCapture)
 });
@@ -26,19 +68,28 @@ function displayText(textData){
   var rawSentences = textData["rawSentences"]
   var changes = textData["changeData"]
   var newText = newSentences.join(".")+"."
-  $("#new_text").text(newText)
+  $("#new_text").html(newText)
+  var editsString = renderEditString(rawSentences, newSentences, changes)
+  $("#edit_window").html(editsString)
+}
+
+function stringSplice(string, index, insertion){
+  return string.slice(0,index)+insertion+string.slice(index)
+};
+
+function renderEditString(rawSentences, newSentences, changeData){
   var editsString = ""
   for(var index in newSentences){
-    if(!!changes[index]){
-      if(changes[index] >0){
+    if(!!changeData[index]){
+      if(changeData[index] >0){
         var insert = newSentences[index]
         insert = stringSplice(insert, 1, "</plus>")
         insert = stringSplice(insert, 0, "<plus>")
         editsString += insert + "."
       } else{
         var insert = rawSentences[index]
-        var spaces = "&nbsp;".repeat(Math.abs(changes[index]))
-        insert = stringSplice(insert, -changes[index], spaces +"</minus>")
+        var spaces = "&nbsp;".repeat(Math.abs(changeData[index]))
+        insert = stringSplice(insert, -changeData[index], spaces +"</minus>")
         insert = stringSplice(insert, 0, "<minus>")
         editsString += insert + "."
       };
@@ -46,11 +97,7 @@ function displayText(textData){
       editsString+= newSentences[index]+"."
     };
   };
-  $("#edit_window").html(editsString)
-}
-
-function stringSplice(string, index, insertion){
-  return string.slice(0,index)+insertion+string.slice(index)
+  return editsString
 };
 
 function shout(data){
